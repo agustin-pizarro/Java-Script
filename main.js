1a25df38f063bbc78ade5c758f03af44a7f6d9f0
¨agus¨ <aguspizarro49@gmail.com>
2022-07-12 18:58:42 -0300

    primer desafio

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-alert ('hola mundo');
+let nombre == "Juan",
+    turnos == 10;
+
+if( nombre == "Juan" ){
+    document.write("Hola" + nombre);   
+}else if (edad"== 10){
+    document.write ("Todavia tenes turnos disponibles")
+}
+
+for(let i = 1; i < 5; i++ ){
+    let nombreUsuario = prompt ("Nombre usuario");
+    alert ("Turno numero: "+i+"Usuario:" +nombreUsuario);
+}
+
+function solicitarHorario (){
+    let horarioIngresado = prompt ("Ingresar horario")
+    alert ("Su proximo horario sera a las " + horarioIngresado)
+}

619f8d17b96341368acaec5fb874c54b0aa4d8a2
¨agus¨ <aguspizarro49@gmail.com>
2022-07-18 20:43:19 -0300

    segundo commit

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-let nombre == "Juan",
-    turnos == 10;
+let nombre1 = "Juan";
+let turnos2 = 2;
 
-if( nombre == "Juan" ){
-    document.write("Hola" + nombre);   
-}else if (edad"== 10){
-    document.write ("Todavia tenes turnos disponibles")
+if (nombre1 == "Juan") {
+    alert("Hola" + nombre1);   
+} else if (edad >= 1) {
+    alert("Todavia tenes turnos disponibles");
+} else if (edad == 0) {
+    alert ("No tenes turnos disponibles");
 }
 
-for(let i = 1; i < 5; i++ ){
+for (let i = 1; i < 5; i++) {
     let nombreUsuario = prompt ("Nombre usuario");
     alert ("Turno numero: "+i+"Usuario:" +nombreUsuario);
 }
 
-function solicitarHorario (){
-    let horarioIngresado = prompt ("Ingresar horario")
-    alert ("Su proximo horario sera a las " + horarioIngresado)
+function solicitarHorario () {
+    let horarioIngresado = prompt ("Ingresar horario");
+    alert ("Su proximo horario sera a las " + horarioIngresado);
 }
+
+let horarioIngresado = 18;

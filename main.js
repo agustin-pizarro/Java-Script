396ccc2c4843d8b0f4d4b98b71bf650fd3190cfa
¨agus¨ <aguspizarro49@gmail.com>
2022-08-02 20:42:35 -0300

    desafio dom

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function iniciarSesion () {
+/* function iniciarSesion () {
     let usuarioIngresado = prompt ("IngresarUsuario");
     alert ("Hola" + usuarioIngresado + "iniciaste sesion")
 }
@@ -23,3 +23,18 @@ const masculino = ["Juan", "Carlos", "Jorge", "Manuel", "Mario"];
 const femenino = ["Carla", "Ana", "Juana", "Sofia", "Maria"];
 const socios = masculino.concat (femenino)
 console.log (socios) 
+*/
+
+/* DOM */
+let titulo = document.getElementsByClassName ("primerTit");
+console.log (titulo.innertText);
+titulo.innertText = "Titulo modificado";
+console.log (titulo.innerText);
+
+let texto = document.getElementByClassName ("segundoTex");
+console.log (texto.innerText);
+texto.innerText = "Texto modificado";
+console.log (texto.innerText);
+
+let textoMain = document.getElemetByClassName ("textoMain");
+textoMain.innerHTML = "<h4> Nuevo h4 en el texto <h4>"

063a892b38f8e180c471d92d43e6cfa57b3e8af0
¨agus¨ <aguspizarro49@gmail.com>
2022-08-09 17:32:07 -0300

    segunda preentrega

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* function iniciarSesion () {
+function iniciarSesion () {
     let usuarioIngresado = prompt ("IngresarUsuario");
     alert ("Hola" + usuarioIngresado + "iniciaste sesion")
 }
@@ -21,9 +21,8 @@ turnosDisponibles ();
 
 const masculino = ["Juan", "Carlos", "Jorge", "Manuel", "Mario"];
 const femenino = ["Carla", "Ana", "Juana", "Sofia", "Maria"];
-const socios = masculino.concat (femenino)
-console.log (socios) 
-*/
+const socios = masculino.concat (femenino);
+console.log (socios);
 
 /* Evento para formulario */
 let formularioTurno = document.getElementByClassName ("formulario");
@@ -47,3 +46,10 @@ console.log (texto.innerText);
 
 let textoMain = document.getElemetByClassName ("textoMain");
 textoMain.innerHTML = "<h4> Nuevo h4 en el texto <h4>"
+
+/* LOCAL STORAGE Y JSON */
+let turnosRestantes = localStorage.getItem ("cantidadTurnos");
+let turnosRestantesJS = JSON.stringify (turnosRestantes);
+
+console.log (turnosRestantes);
+console.log (turnosRestantesJS);
